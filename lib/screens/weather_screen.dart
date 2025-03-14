dec00f3ca4ccb76af4ea2c31dbacc9083bbf1035
FADRIQUE_MOLINA\josem <joeydicastro@gmail.com>
2025-03-13 21:56:20 -0700

    using future builder to show weather on screen

--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -1,18 +1,41 @@
-import 'package:deep_wheatherapp/providers/weather_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import '../providers/weather_provider.dart';
 
 class WeatherScreen extends StatelessWidget {
   const WeatherScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final weatherProvider = Provider.of<WeatherProvider>(context);
-    weatherProvider.fetchWeatherData();
+    final weatherProvider = Provider.of<WeatherProvider>(
+      context,
+      listen: false,
+    );
+    final future = weatherProvider.fetchWeatherData();
 
     return Scaffold(
-      appBar: AppBar(title: Text("App del clima")),
-      body: Center(child: Text(weatherProvider.weatherData)),
+      appBar: AppBar(title: const Text('App del Clima')),
+      body: FutureBuilder<void>(
+        future: future,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            return Consumer<WeatherProvider>(
+              builder: (context, weatherProvider, child) {
+                return Center(
+                  child: Text(
+                    weatherProvider.weatherData,
+                    style: const TextStyle(fontSize: 24),
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
     );
   }
 }
